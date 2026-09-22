d34d9075ab1036f1b1f50f68f5a90a8bf6351333
sdhrmnt <sdhrmnt1105@gmail.com>
2026-09-22 10:42:50 +0700

    chore: perubahan di express, add cors and update SPEC.md

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcryptjs')
 const {
   Model
 } = require('sequelize');
@@ -10,19 +11,54 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      User.hasOne(models.PatientProfile, { foreignKey: 'UserId' })
+      User.hasOne(models.DoctorProfile, { foreignKey: 'UserId' })
       // define association here
     }
   }
   User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING,
-    name: DataTypes.STRING,
-    timezone: DataTypes.STRING,
-    isActive: DataTypes.BOOLEAN
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { isEmail: true },
+
+    },
+    password: {
+      TYPE: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM('patient', 'doctor', 'admin'),
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { notEmpty: true }
+    },
+    phone: DataTypes.STRING,
+    timezone: {
+      type: DataTypes.STRING,
+      defaultValue: 'Asia/Jakarta'
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
   }, {
     sequelize,
     modelName: 'User',
+    hooks: {
+      beforeCreate: async (user) => {
+        user.password = await bcrypt.hash(user.password, 10)
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          user.password = await bcrypt.hash(user.password, 10)
+        }
+      }
+    }
   });
   return User;
 };
