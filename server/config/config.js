096ed1d572c1e7735d0a2dee71f2233db3c1eb1a
sdhrmnt <sdhrmnt1105@gmail.com>
2026-09-23 20:21:02 +0700

    fix(db):  config test

--- server/config/config.js
+++ server/config/config.js
@@ -12,9 +12,10 @@ module.exports = {
     test: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: 'ur-doc-test',
+        database: process.env.DB_NAME_TEST || 'ur-doc-test',
         host: process.env.DB_HOST,
         dialect: 'postgres',
+
     },
     production: {
         username: process.env.DB_USER,
